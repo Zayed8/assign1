c7068cf6faf7f2262c7357d58f09342a6544021e
Zayed8 <138813969+Zayed8@users.noreply.github.com>
2023-08-03 21:33:21 +0530

    Update pday1.py

--- pday1.py
+++ pday1.py
@@ -1,10 +1,7 @@
-list = []
-nums = int(input("Enter the number of Elements :"))
-sum = 0
-for i in range(0,nums):
-        elements = int(input("Nos :"))
-        list.append(elements)
-        sum += elements
-        average = sum/nums
-print(list)
+num1 = int(input("Enter the number of Elements :"))
+num2 = int(input("Enter the number of Elements :"))
+num3 = int(input("Enter the number of Elements :"))
+sum = num1+num2+num3
+average = sum/3
+print(average)
 print("Average of",elements,"is :",average)
