1dcfbb87b889d8f0bb149cf4e6d393dacb81b951
Zayed8 <138813969+Zayed8@users.noreply.github.com>
2023-08-06 16:28:44 +0530

    Update pday1.py

--- pday1.py
+++ pday1.py
@@ -4,4 +4,4 @@ num3 = int(input("Enter the number of Elements :"))
 sum = num1+num2+num3
 average = sum/3
 print(average)
-print("Average of",elements,"is :",average)
+print("The Average of",num1, num2, "and", num3 ,"is :",average)
